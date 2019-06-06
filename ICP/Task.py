6e0dcf69fa15d6f2f78c220e0af55bcf1529634e
tiwariaanchal <49729337+tiwariaanchal@users.noreply.github.com>
2019-06-06 13:31:42 -0500

    icp1

---
+++ ICP/Task.py
@@ -0,0 +1,16 @@
+inp=input('enter an string :')
+new_inp=inp[:-2]
+print(new_inp)
+def reverse(string):
+    string = string[::-1]
+    return string
+
+print(reverse(new_inp))
+num1=input('Give me an number')
+num2=input('Give me another number')
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+
+inp1=input('Enter an :')
+print(inp1.replace("python","pythons"))
